f5777e132f070fabeeabb156288ef54460baa60b
randigtroja <randigtroja@users.noreply.github.com>
2017-07-14 21:23:28 +0200

    grundstomme för privata meddelanden

---
+++ Flashback.Model/PrivateMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Flashback.Model
+{
+    public class PrivateMessage
+    {                  
+        public string Id { get; set; }            
+        public string Token { get; set; }            
+        public string Name { get; set; }            
+        public string FromName { get; set; }            
+        public DateTime Date { get; set; }            
+        public string Message { get; set; }         
+        public bool IsUnread { get; set; }        
+    }
+}
---
+++ Flashback.Services/Messages/MessagesService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Flashback.Model;
+
+namespace Flashback.Services.Messages
+{
+    public class MessagesService
+    {
+        private readonly FlashbackHttpClient _httpClient;
+
+        public MessagesService(CookieContainer container)
+        {
+            _httpClient = new FlashbackHttpClient(container);
+        }
+
+        public async Task<List<PrivateMessage>> GetPrivateMessages()
+        {
+            var result = await _httpClient.GetStringAsync("https://www.flashback.org/private.php");
+            var messages = await ParsePrivateMessages(result);
+            
+            return messages;
+        }
+
+        private async Task<List<PrivateMessage>> ParsePrivateMessages(string result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
