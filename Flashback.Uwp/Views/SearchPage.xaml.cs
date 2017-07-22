ad80cba6144680bb0adfd68205c255c602fced7a
randigtroja <randigtroja@users.noreply.github.com>
2017-07-22 21:20:51 +0200

    cache på söksidan
    måste ha cache påslaget, annars måste man söka
    om varenda gång man backar ut från framsökt tråd

--- Flashback.Uwp/Views/SearchPage.xaml.cs
+++ Flashback.Uwp/Views/SearchPage.xaml.cs
@@ -25,6 +25,7 @@ namespace FlashbackUwp.Views
         public SearchPage()
         {
             this.InitializeComponent();
+           NavigationCacheMode = NavigationCacheMode.Enabled;
         }
     }
 }
