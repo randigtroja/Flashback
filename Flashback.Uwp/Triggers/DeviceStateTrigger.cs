f2c4273e17c6972730ebc486dff55f1717e643b0
randigtroja <randigtroja@users.noreply.github.com>
2017-07-29 21:25:26 +0200

    statetrigger för device
    ska användas för framtida mobil-anpassningar

---
+++ Flashback.Uwp/Triggers/DeviceStateTrigger.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml;
+
+namespace FlashbackUwp.Triggers
+{
+    public class DeviceStateTrigger : StateTriggerBase
+    {
+        private string _deviceFamily;
+
+        public string DeviceFamily
+        {
+            get { return _deviceFamily; }
+            set
+            {
+                _deviceFamily = value;
+                SetActive(_deviceFamily == Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily);
+            }
+        }
+    }
+}
