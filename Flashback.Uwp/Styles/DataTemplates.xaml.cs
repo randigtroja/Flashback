27153b97bf1f17ac5a27a935efe1e6dc7fd056c5
flashsharp <magro.a@outlook.com>
2017-08-27 15:10:00 +0200

    DataTemplate som StaticResource (#73)
    * Create DataTemplates.xaml
    
    * Create DataTemplates.cs
    
    * Update App.xaml
    
    * Update Flashback.Uwp.csproj
    
    * Update AktuellaAmnenPage.xaml
    
    * DataTemplates.cs
    
    * Update DataTemplates.xaml
    
    * Update NewPostsPage.xaml
    
    * Update NewTopicsPage.xaml
    
    * Update NewPostsPage.xaml
    
    * Update NewTopicsPage.xaml
    
    * Update MyStartedThreadsPage.xaml
    
    * Update SearchPage.xaml
    
    * Rename DataTemplates.cs to DataTemplates.xaml.cs
    
    * Update Flashback.Uwp.csproj

---
+++ Flashback.Uwp/Styles/DataTemplates.xaml.cs
@@ -0,0 +1,10 @@
+namespace FlashbackUwp.Styles
+{
+    public partial class DataTemplates
+    {
+        public DataTemplates ()
+        {
+            InitializeComponent();  // Nödvändigt för att x:Bind ska fungera ihop med StaticResource
+        }
+    }
+}
