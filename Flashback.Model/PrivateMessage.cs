e369ad59a1bf0e25c5289f6cf35b800d597e7496
flashsharp <magro.a@outlook.com>
2017-08-28 20:15:30 +0200

    StaticResource för PrivateMessagesPage.xaml  (#77)
    * Update PrivateMessage.cs
    
    * Update DataTemplates.xaml
    
    * Update PrivateMessagesPage.xaml
    
    * Update PrivateMessagesPage.xaml
    
    * Update DataTemplates.xaml
    
    * Update PrivateMessage.cs

--- Flashback.Model/PrivateMessage.cs
+++ Flashback.Model/PrivateMessage.cs
@@ -1,17 +1,19 @@
-﻿using System;
+using System;
 
 namespace Flashback.Model
 {
     public class PrivateMessage
     {                  
-        public string Id { get; set; }            
-        public string Token { get; set; }            
-        public string Name { get; set; }            
-        public string FromName { get; set; }            
-        public DateTime Date { get; set; }            
-        public string Message { get; set; }         
+        public string Id { get; set; }
+        public string Token { get; set; } 
+        public string Name { get; set; }
+        public string FromName { get; set; }
+        public DateTime Date { get; set; }
+        public string Message { get; set; }
         public bool IsUnread { get; set; }
         public string FolderId { get; set; }
         public string Title { get; set; }
+
+        public string XamlCode => IsUnread ? "\uE715" : "\uE8C3";
     }
 }
