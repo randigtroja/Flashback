a36bb09c6b72fa3ff924d80e84c214d7a7d10db4
randigtroja <randigtroja@users.noreply.github.com>
2017-08-31 20:52:07 +0200

    rensning av skit

--- Flashback.Uwp/Views/ForumMainList.xaml.cs
+++ Flashback.Uwp/Views/ForumMainList.xaml.cs
@@ -10,15 +10,6 @@ namespace FlashbackUwp.Views
         {
             this.InitializeComponent();
             NavigationCacheMode = NavigationCacheMode.Disabled;
-
-            //if (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile")
-            //{
-            //    Shell.HamburgerMenu.HamburgerButtonVisibility = Visibility.Collapsed;
-            //    Shell.HamburgerMenu.VisualStateWideDisplayMode = SplitViewDisplayMode.Overlay;
-            //    Shell.HamburgerMenu.VisualStateNormalDisplayMode = SplitViewDisplayMode.Overlay;
-            //    Shell.HamburgerMenu.VisualStateNarrowDisplayMode = SplitViewDisplayMode.Overlay;
-            //}
-            
         }
     }
 }
