238616cfe514517af893b1353d4d8de78bf50920
flashsharp <magro.a@outlook.com>
2017-09-06 21:06:15 +0200

    Update HtmlWebViewExtender.cs

--- Flashback.Uwp/Extensions/HtmlWebViewExtender.cs
+++ Flashback.Uwp/Extensions/HtmlWebViewExtender.cs
@@ -1,4 +1,4 @@
-﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace FlashbackUwp.Extensions
@@ -8,23 +8,14 @@ namespace FlashbackUwp.Extensions
     /// </summary>
     public class HtmlWebViewExtender
     {
-        public static string GetHTML(DependencyObject obj)
-        {
-            return (string)obj.GetValue(HTMLProperty);
-        }
-
-        public static void SetHTML(DependencyObject obj, string value)
-        {
-            obj.SetValue(HTMLProperty, value);
-        }
+        public static string GetHTML(DependencyObject obj) => (string)obj.GetValue(HTMLProperty);
+        public static void SetHTML(DependencyObject obj, string value) => obj.SetValue(HTMLProperty, value);
 
         public static readonly DependencyProperty HTMLProperty = DependencyProperty.RegisterAttached("HTML", typeof(string), typeof(HtmlWebViewExtender), new PropertyMetadata("", new PropertyChangedCallback(OnHTMLChanged)));
 
         private static void OnHTMLChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-
-            WebView wv = d as WebView;
-            if (wv != null)
+            if (d is WebView wv)
             {
                 wv.NavigateToString((string)e.NewValue);
             }
