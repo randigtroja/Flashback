72a986571d127200c0852deac1a0acd4fb9f7b22
flashsharp <magro.a@outlook.com>
2017-09-07 21:35:45 +0200

    Update HtmlRenderOptions.cs (#95)

--- Flashback.Model/HtmlRenderOptions.cs
+++ Flashback.Model/HtmlRenderOptions.cs
@@ -23,22 +23,29 @@ namespace Flashback.Model
 
         public string ReplaceSmileys(string postMessage)
         {
+            void _replace(string fileName, string title, string emoticon)
+            {
+                postMessage = postMessage.Replace(
+                    $"<img src=\"https://static.flashback.org/img/smilies2/{fileName}\" border=\"0\" alt=\"\" title=\"{title}\" class=\"inlineimg\">",
+                    $"<span style='font-family: Segoe UI Emoji;'>{emoticon}</span>");
+            }
+
             // Todo: Fyll på med fler replace
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/wink.gif\" border=\"0\" alt=\"\" title=\"Whink\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128521;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/sad.gif\" border=\"0\" alt=\"\" title=\"Sad\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128543;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/noexpression.gif\" border=\"0\" alt=\"\" title=\"Noexpression\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128529;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/cool2.gif\" border=\"0\" alt=\"\" title=\"Cool\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128526;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/skamsen.gif\" border=\"0\" alt=\"\" title=\"Skamsen\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128563;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/sad44.gif\" border=\"0\" alt=\"\" title=\"Sad44\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128557;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/unsure.gif\" border=\"0\" alt=\"\" title=\"Unsure\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128533;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/grin.gif\" border=\"0\" alt=\"\" title=\"Grin\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128512;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/tongue.gif\" border=\"0\" alt=\"\" title=\"Tongue\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128539;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/smile1.gif\" border=\"0\" alt=\"\" title=\"Smile\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#x1F642;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/laugh.gif\" border=\"0\" alt=\"\" title=\"Laugh\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128516;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/cry.gif\" border=\"0\" alt=\"\" title=\"Cry\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128546;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/ohmy.gif\" border=\"0\" alt=\"\" title=\"Ohmy\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128559;</span>");
-            postMessage = postMessage.Replace("<img src=\"https://static.flashback.org/img/smilies2/evilgrin39.gif\" border=\"0\" alt=\"\" title=\"Evilgrin39\" class=\"inlineimg\">", "<span style=\"font-family: Segoe UI Emoji;\">&#128520;</span>");
-            
+            _replace("wink.gif", "Whink", "&#128521;");
+            _replace("sad.gif", "Sad", "&#128543;");
+            _replace("noexpression.gif", "Noexpression", "&#128529;");
+            _replace("cool2.gif", "Cool", "&#128526;");
+            _replace("skamsen.gif", "Skamsen", "&#128563;");
+            _replace("sad44.gif", "Sad44", "&#128557;");
+            _replace("unsure.gif", "Unsure", "&#128533;");
+            _replace("grin.gif", "Grin", "&#128512;");
+            _replace("tongue.gif", "Tongue", "&#128539;");
+            _replace("smile1.gif", "Smile", "&#x1F642;");
+            _replace("laugh.gif", "Laugh", "&#128516;");
+            _replace("cry.gif", "Cry", "&#128546;");
+            _replace("ohmy.gif", "Ohmy", "&#128559;");
+            _replace("evilgrin39.gif", "Evilgrin39", "&#128520;");
+
             return postMessage;           
         }
 
