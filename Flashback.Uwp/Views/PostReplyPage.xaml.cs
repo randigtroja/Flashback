a89460280dccf1aee64a916538ceec3e6272ffb0
flashsharp <magro.a@outlook.com>
2017-09-10 19:00:12 +0200

    Update PostReplyPage.xaml.cs

--- Flashback.Uwp/Views/PostReplyPage.xaml.cs
+++ Flashback.Uwp/Views/PostReplyPage.xaml.cs
@@ -1,10 +1,9 @@
 using Windows.UI.Xaml.Controls;
 
-
 namespace FlashbackUwp.Views
 {   
     public sealed partial class PostReplyPage : Page
     {
-        public PostReplyPage() => this.InitializeComponent();
+        public PostReplyPage() => InitializeComponent();
     }
 }
