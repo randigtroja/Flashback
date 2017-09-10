9960321b911d5f69ba39be83554809da80271ef9
flashsharp <magro.a@outlook.com>
2017-09-10 19:01:45 +0200

    Update SearchPage.xaml.cs

--- Flashback.Uwp/Views/SearchPage.xaml.cs
+++ Flashback.Uwp/Views/SearchPage.xaml.cs
@@ -1,14 +1,13 @@
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-
 namespace FlashbackUwp.Views
 {
     public sealed partial class SearchPage : Page
     {
         public SearchPage()
         {
-           this.InitializeComponent();
+           InitializeComponent();
            NavigationCacheMode = NavigationCacheMode.Enabled;
         }
     }
